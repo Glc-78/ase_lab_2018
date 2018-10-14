402140f59e13e9fad5c283d165586969851550c4
Glc-78 <caramelleamare@gmail.com>
2018-10-14 03:30:00 +0200

    stuffs

--- src/calculator.py
+++ src/calculator.py
@@ -1,63 +1,47 @@
+# ase_lab
 
-def mysum(m,n):
-    res = m
-    if n<0:
-        for i in range(0, -n):
-            res -= 1
+def sum(m, n):
+    result = m
+    if n < 0:
+        for i in range(abs(n)):
+            result -= 1
     else:
-        for i in range(0,n):
-            res+=1
+        for i in range(n):
+            result += 1
+    return result
 
-    return res
-
-#a=21
-#b=-3
-#print("sum->",a,"+",b,"=",mysum(a,b))
-
-def divide(m,n):
-    res=0
-    sign=1
-    if(m<0):
-        m = abs(m)
-        sign = sign * (-1)
-    if (n < 0):
-        n=abs(n)
-        sign = sign * (-1)
-    while(m>=0):
-        m=m-n
-        if(m>=0): # asd
-            res+=1
-    return res*sign
-
-#a=-21
-#b=3
-#print("divide->",a,":",b,"=",divide(a,b))
-
-def subtract(m,n):
-    res = m
-    if n > 0:
-        for i in range(0, n):
-            res -= 1
-    else:
-        for i in range(0, -n):
-            res += 1
-
-    return res
-
-def multiply(m,n):
-    if(m==0 or n==0):
-        return 0
-
-    res = 0
+def divide(m, n):
+    result = 0
     sign = 1
-    if (m < 0):
-        m = abs(m)
-        sign = sign * (-1)
-    if (n < 0):
-        n = abs(n)
-        sign = sign * (-1)
-    for i in range(n):
-        res+=m
-    return res * sign
+    if n == 0:
+        raise ZeroDivisionError("You cannot divide by 0!")
+    if (n < 0 and m > 0) or (n > 0 and m <0):
+        sign = -1
+    n = abs(n)
+    m = abs(m)
+    while m-n >= 0:
+        m -= n
+        result += 1
+    return sign * result
 
-print("Fine")
+def subtract(m, n):
+    result = m
+    if n < 0:
+        for i in range(abs(n)):
+            result += 1
+    else:
+        for i in range(abs(n)):
+            result -= 1
+    return result
+
+def multiply(m, n):
+    sign = 1
+    if n == 0 or m == 0:
+        return 0
+    if (m > 0 and n < 0) or (m < 0 and n > 0):
+        sign = -1
+    result = m = abs(m)
+    n = abs(n)
+    for i in range(n-1):
+        result += m
+    return sign * result
